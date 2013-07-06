baff9b35745ebb8dc87ab0c53977e515029cb682
Marc-Elian Begin <meb@sixsq.com>
2013-07-06 16:33:06 +0200

    Refactored authz logic removing duplication

--- src/slipstream/ui/views/authz.clj
+++ src/slipstream/ui/views/authz.clj
@@ -3,7 +3,6 @@
             [slipstream.ui.models.authz :as authz-model]
             [slipstream.ui.models.module :as module-model]))
 
-(def authorization-view-template-html "slipstream/ui/views/authorization-view.html")
 (def authorization-edit-template-html "slipstream/ui/views/authorization-edit.html")
 
 (def authorization-sel [:#authorization])
@@ -20,3 +19,123 @@
   (if (true? (acl? authz))
     (html/set-attr :checked "true")
     (html/remove-attr :checked)))
+
+(html/defsnippet authz-view-part-snip authorization-edit-template-html authorization-view-sel
+  [authz]
+  [[:td (html/nth-of-type 2)] :input]
+  (assign-checked authz-model/ownerget? authz)
+  [[:td (html/nth-of-type 3)] :input]
+  (assign-checked authz-model/groupget? authz)
+  [[:td (html/nth-of-type 4)] :input]
+  (assign-checked authz-model/publicget? authz))
+
+(html/defsnippet authz-edit-part-snip authorization-edit-template-html authorization-edit-sel
+  [authz]
+  [[:td (html/nth-of-type 2)] :input]
+  (assign-checked authz-model/ownerput? authz)
+  [[:td (html/nth-of-type 3)] :input]
+  (assign-checked authz-model/groupput? authz)
+  [[:td (html/nth-of-type 4)] :input]
+  (assign-checked authz-model/publicput? authz))
+
+(html/defsnippet authz-delete-part-snip authorization-edit-template-html authorization-delete-sel
+  [authz]
+  [[:td (html/nth-of-type 2)] :input]
+  (assign-checked authz-model/ownerdelete? authz)
+  [[:td (html/nth-of-type 3)] :input]
+  (assign-checked authz-model/groupdelete? authz)
+  [[:td (html/nth-of-type 4)] :input]
+  (assign-checked authz-model/publicdelete? authz))
+
+(html/defsnippet authz-post-part-snip authorization-edit-template-html authorization-execute-sel
+  [authz]
+  [[:td (html/nth-of-type 2)] :input]
+  (assign-checked authz-model/ownerpost? authz)
+  [[:td (html/nth-of-type 3)] :input]
+  (assign-checked authz-model/grouppost? authz)
+  [[:td (html/nth-of-type 4)] :input]
+  (assign-checked authz-model/publicpost? authz))
+
+(html/defsnippet authz-children-part-snip authorization-edit-template-html authorization-create-children-sel
+  [authz]
+  [[:td (html/nth-of-type 2)] :input]
+  (assign-checked authz-model/ownercreatechildren? authz)
+  [[:td (html/nth-of-type 3)] :input]
+  (assign-checked authz-model/groupcreatechildren? authz)
+  [[:td (html/nth-of-type 4)] :input]
+  (assign-checked authz-model/publiccreatechildren? authz))
+
+(html/defsnippet authz-group-part-snip authorization-edit-template-html authorization-inheritance-sel
+  [authz]
+  [[:td (html/nth-of-type 2)] :input]
+  (assign-checked authz-model/ownercreatechildren? authz)
+  [[:td (html/nth-of-type 3)] :input]
+  (assign-checked authz-model/groupcreatechildren? authz)
+  [[:td (html/nth-of-type 4)] :input]
+  (assign-checked authz-model/publiccreatechildren? authz))
+
+(html/defsnippet authz-project-snip authorization-edit-template-html authorization-acl-sel
+  [authz]
+  authorization-view-sel (html/substitute (authz-view-part-snip authz))
+  authorization-edit-sel (html/substitute (authz-edit-part-snip authz))
+  authorization-delete-sel (html/substitute (authz-delete-part-snip authz))
+  authorization-create-children-sel (html/substitute (authz-children-part-snip authz))
+  authorization-execute-sel nil)
+
+(html/defsnippet authz-image-snip authorization-edit-template-html authorization-acl-sel
+  [authz]
+  authorization-view-sel (html/substitute (authz-view-part-snip authz))
+  authorization-edit-sel (html/substitute (authz-edit-part-snip authz))
+  authorization-delete-sel (html/substitute (authz-delete-part-snip authz))
+  authorization-execute-sel (html/substitute (authz-post-part-snip authz))
+  [authorization-execute-sel :> [:td html/first-of-type]] (html/content "Build or Run Single Image")
+  authorization-create-children-sel nil)
+
+(html/defsnippet authz-deployment-snip authorization-edit-template-html authorization-acl-sel
+  [authz]
+  authorization-view-sel (html/substitute (authz-view-part-snip authz))
+  authorization-edit-sel (html/substitute (authz-edit-part-snip authz))
+  authorization-delete-sel (html/substitute (authz-delete-part-snip authz))
+  [authorization-execute-sel :> [:td html/first-of-type]] (html/content "Run Deployment"))
+
+(html/defsnippet authz-inhertance-snip authorization-edit-template-html authorization-inheritance-sel
+  [authz]
+  [:#authz-inherited :td :input]
+  (assign-checked authz-model/inherited? authz)
+  [:#authz-group :td]
+  (html/content (authz-model/group authz)))
+
+(defmulti authz-by-category
+  (fn [authz category]
+    category))
+
+(defmethod authz-by-category "Project"
+  [authz category]
+    (authz-project-snip authz))
+
+(defmethod authz-by-category "Image"
+  [authz category]
+    (authz-image-snip authz))
+
+(defmethod authz-by-category "Deployment"
+  [authz category]
+    (authz-deployment-snip authz))
+
+(html/defsnippet authz-edit-snip authorization-edit-template-html authorization-sel
+  [module]
+  authorization-acl-sel
+  (let [category (:category (module-model/attrs module))]
+    (html/substitute
+      (authz-by-category
+        (authz-model/authz module)
+        category)))
+  authorization-inheritance-sel 
+  (html/substitute
+    (authz-inhertance-snip
+      (authz-model/authz module))))
+
+(html/defsnippet authz-view-snip authorization-edit-template-html authorization-sel
+  [module]
+  authorization-sel (html/substitute (authz-edit-snip module))
+  ; Only difference for view compaired with edit is that we disable all input elements
+  [authorization-sel :input] (html/set-attr :disabled "disabled"))
