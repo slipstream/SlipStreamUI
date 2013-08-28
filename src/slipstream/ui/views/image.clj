588491492c067e9b57db878e650d41dc7104b6b5
Marc-Elian Begin <meb@sixsq.com>
2013-08-28 16:37:55 +0200

    Setting module name in input reference element instead of uri

--- src/slipstream/ui/views/image.clj
+++ src/slipstream/ui/views/image.clj
@@ -301,7 +301,7 @@
       (html/set-attr :value (:loginuser (module-model/attrs module)))
       (common/set-disabled (not (module-model/base? module))))
 
-  [image-reference-sel] (html/set-attr :value (:modulereferenceuri (module-model/attrs module)))
+  [image-reference-sel] (html/set-attr :value (common-model/elem-name module))
   [:#moduleReferenceChooser] (common/set-disabled (module-model/base? module))
   
   image-cloud-configuration-sel
