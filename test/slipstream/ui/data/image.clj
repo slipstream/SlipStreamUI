8442672e9c86b9ae7812033c7207c5ef3b0e5c26
Marc-Elian Begin <meb@sixsq.com>
2013-09-12 17:05:31 +0200

    Super is super

--- test/slipstream/ui/data/image.clj
+++ test/slipstream/ui/data/image.clj
@@ -90,5 +90,5 @@
 <cloudImageIdentifier cloudImageIdentifier='abc' cloudServiceName='my-cloud' resourceUri='module/Public/BaseImages/Ubuntu/12.04/4/stratuslab'></cloudImageIdentifier>
 </cloudImageIdentifiers>
 <extraDisks class='org.hibernate.collection.PersistentBag'></extraDisks>
-<user issuper='false' resourceUri='user/super' name='super'></user>
+<user issuper='true' resourceUri='user/super' name='super'></user>
 </imageModule>")))
