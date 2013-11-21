f8559f8d37e0be7f2a5b23c17f80e73a475b9a2d
zyegfryed <zyegfryedgmail.com>
2013-11-21 11:55:57 +0100

    Refactored series processing

--- src/slipstream/ui/views/js/metering.js
+++ src/slipstream/ui/views/js/metering.js
@@ -10,46 +10,58 @@
             }
         }, options );
 
+        function get_series(samples) {
+            var series = _gen_series(samples);
+            return [ _get_current_series(series), _get_past_series(series) ];
+        }
+
+        function _gen_series(samples) {
+            var series = {};
+            $.each(samples, function(index, sample) {
+                var key = sample.groupby.source,
+                    serie = [ sample.period_start * 1000.0, sample.sum ];
+                if (key in series) {
+                    series[key].push(serie);
+                } else {
+                    series[key] = Array(serie);
+                }
+            });
+            return series;
+        }
+
+        function _get_past_series(series) {
+            var _series = [];
+            $.each(series, function(service, series) {
+                _series.push({ label: service, data: series });
+            });
+            return _series;
+        }
+
+        function _get_current_series(series) {
+            var _series = [];
+            $.each(series, function(service, series) {
+                var last_idx = series.length - 1;
+                _series.push({ label: service, data: series[last_idx][1] });
+            });
+            return _series;
+        }
+
         return this.each(function(elem) {
             $.ajax({
                 url: settings.url.replace("<meter>", settings.meter),
                 data: settings.params,
                 context: this,
                 success: function(samples, status, xhr) {
-                    //---------------------------------------------------------
-                    // Data coputation
-                    //---------------------------------------------------------
-                    var series = {};
-                    $.each(samples, function(index, sample) {
-                        var key = sample.groupby.source,
-                            serie = [ sample.period_start * 1000.0, sample.sum ];
-                        if (key in series) {
-                            series[key].push(serie);
-                        } else {
-                            series[key] = Array(serie);
-                        }
-                    });
-
-                    var lineData = [];
-                    $.each(series, function(service, series) {
-                        lineData.push({ label: service, data: series });
-                    });
-
-                    var pieData = [];
-                    $.each(series, function(service, series) {
-                        var last_idx = series.length - 1;
-                        pieData.push({ label: service, data: series[last_idx][1] });
-                    });
-
-                    //---------------------------------------------------------
-                    // UI refresh
-                    //---------------------------------------------------------
+                    var series = get_series(samples);
                     var $this = $(this);
                     $this.empty();
 
-                    $('<div class="col1" width="200" height="215"></div><div class="col2" width="590" height="215"></div><div class="col3"></div>').appendTo($this);
+                    $('<div class="col1"></div><div class="col2"></div><div class="col3"></div>').appendTo($this);
 
-                    $(".col1", $this).plot(pieData, {
+                    $(".col1", $this).css({
+                        width: 200,
+                        height: 215
+                    }).plot(series[0], {
                         series: {
                             pie: {
                                 radius: 0.75,
@@ -72,19 +84,22 @@
                         colors: settings.colors
                     });
 
-                    $(".col2", $this).plot(lineData, {
+                    $(".col2", $this).css({
+                        width: 590,
+                        height: 215
+                    }).plot(series[1], {
                         xaxis: { mode: "time" },
                         stack: true,
                         lines: {
                             show: true,
-                            fill:true
+                            fill: true
                         },
                         legend: {
                             sorted: true ,
                             container: $(".col3", $this),
                         },
                         points: { show: true },
-                        grid: { hoverable: true },
+                        grid: { hoverable: true, borderWidth: 0 },
                         tooltip: true,
                         tooltipOpts: {
                             content: "%s: %y",
