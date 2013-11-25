0e51cc651e0a0be2336c03d65496f615f8aaf307
Marc-Elian Begin <meb@sixsq.com>
2013-11-25 20:37:55 +0100

    Updated key values

--- test/slipstream/ui/data/welcome.clj
+++ test/slipstream/ui/data/welcome.clj
@@ -16,92 +16,92 @@
       </item>
    </modules>
    <serviceCatalogues>
-      <serviceCatalog deleted='false' resourceUri='servicecatalog/loco' cloud='loco' creation='2013-11-12 20:21:20.192 CET'>
+      <serviceCatalog deleted='false' resourceUri='servicecatalog/cloudstack' cloud='cloudstack' creation='2013-11-12 20:21:20.192 CET'>
          <parameters class='org.hibernate.collection.PersistentMap'>
             <entry>
-               <string><![CDATA[support.email]]></string>
-               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='support.email' description='Support email adress for this cloud service' category='General' mandatory='false' type='String' readonly='false' catalogCategory='General'>
+               <string><![CDATA[cloudstack.support.email]]></string>
+               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='cloudstack.support.email' description='Support email adress for this cloud service' category='General' mandatory='false' type='String' readonly='false' catalogCategory='General'>
                   <instructions><![CDATA[]]></instructions>
                   <value><![CDATA[]]></value>
                </parameter>
             </entry>
             <entry>
-               <string><![CDATA[single.vm.max.cpu]]></string>
-               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='single.vm.max.cpu' description='Maximum number of CPUs (cores) for a single VM' category='General' mandatory='false' type='String' readonly='false' catalogCategory='Single_VM'>
+               <string><![CDATA[cloudstack.single.vm.max.cpu]]></string>
+               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='cloudstack.single.vm.max.cpu' description='Maximum number of CPUs (cores) for a single VM' category='Single VM Capacity' mandatory='false' type='String' readonly='false' catalogCategory='Single_VM'>
                   <instructions><![CDATA[]]></instructions>
                   <value><![CDATA[]]></value>
                </parameter>
             </entry>
             <entry>
-               <string><![CDATA[overall.ram]]></string>
-               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='overall.ram' description='Overall available RAM' category='General' mandatory='false' type='String' readonly='false' catalogCategory='Overall'>
+               <string><![CDATA[cloudstack.overall.ram]]></string>
+               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='cloudstack.overall.ram' description='Overall available RAM' category='Overall Capacity' mandatory='false' type='String' readonly='false' catalogCategory='Overall'>
                   <instructions><![CDATA[]]></instructions>
                   <value><![CDATA[]]></value>
                </parameter>
             </entry>
             <entry>
-               <string><![CDATA[overall.cpu]]></string>
-               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='overall.cpu' description='Overall available CPUs (cores)' category='General' mandatory='false' type='String' readonly='false' catalogCategory='Overall'>
+               <string><![CDATA[cloudstack.overall.cpu]]></string>
+               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='cloudstack.overall.cpu' description='Overall available CPUs (cores)' category='Overall Capacity' mandatory='false' type='String' readonly='false' catalogCategory='Overall'>
                   <instructions><![CDATA[]]></instructions>
                   <value><![CDATA[]]></value>
                </parameter>
             </entry>
             <entry>
-               <string><![CDATA[single.vm.max.ram]]></string>
-               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='single.vm.max.ram' description='Maximum number of RAM (GB) for a single VM' category='General' mandatory='false' type='String' readonly='false' catalogCategory='Single_VM'>
+               <string><![CDATA[cloudstack.single.vm.max.ram]]></string>
+               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='cloudstack.single.vm.max.ram' description='Maximum number of RAM (GB) for a single VM' category='Single VM Capacity' mandatory='false' type='String' readonly='false' catalogCategory='Single_VM'>
                   <instructions><![CDATA[]]></instructions>
                   <value><![CDATA[]]></value>
                </parameter>
             </entry>
             <entry>
-               <string><![CDATA[general.description]]></string>
-               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='general.description' description='General description of the cloud service (including optional further links)' category='General' mandatory='false' type='String' readonly='false' catalogCategory='General'>
+               <string><![CDATA[cloudstack.general.description]]></string>
+               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='cloudstack.general.description' description='General description of the cloud service (including optional further links)' category='General' mandatory='false' type='String' readonly='false' catalogCategory='General'>
                   <instructions><![CDATA[]]></instructions>
                   <value><![CDATA[]]></value>
                </parameter>
             </entry>
          </parameters>
       </serviceCatalog>
-      <serviceCatalog deleted='false' resourceUri='servicecatalog/toto' cloud='toto' creation='2013-11-12 20:21:20.98 CET'>
+      <serviceCatalog deleted='false' resourceUri='servicecatalog/stratuslab' cloud='stratuslab' creation='2013-11-12 20:21:20.98 CET'>
          <parameters class='org.hibernate.collection.PersistentMap'>
             <entry>
-               <string><![CDATA[support.email]]></string>
-               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='support.email' description='Support email adress for this cloud service' category='General' mandatory='false' type='String' readonly='false' catalogCategory='General'>
+               <string><![CDATA[stratuslab.support.email]]></string>
+               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='stratuslab.support.email' description='Support email adress for this cloud service' category='General' mandatory='false' type='String' readonly='false' catalogCategory='General'>
                   <instructions><![CDATA[]]></instructions>
                   <value><![CDATA[mickey@disney.com]]></value>
                </parameter>
             </entry>
             <entry>
-               <string><![CDATA[single.vm.max.cpu]]></string>
-               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='single.vm.max.cpu' description='Maximum number of CPUs (cores) for a single VM' category='General' mandatory='false' type='String' readonly='false' catalogCategory='Single_VM'>
+               <string><![CDATA[stratuslab.single.vm.max.cpu]]></string>
+               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='stratuslab.single.vm.max.cpu' description='Maximum number of CPUs (cores) for a single VM' category='General' mandatory='false' type='String' readonly='false' catalogCategory='Single_VM'>
                   <instructions><![CDATA[You won't be able to provision more for a single VM]]></instructions>
                   <value><![CDATA[]]></value>
                </parameter>
             </entry>
             <entry>
-               <string><![CDATA[overall.ram]]></string>
-               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='overall.ram' description='Overall available RAM' category='General' mandatory='false' type='String' readonly='false' catalogCategory='Overall'>
+               <string><![CDATA[stratuslab.overall.ram]]></string>
+               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='stratuslab.overall.ram' description='Overall available RAM' category='General' mandatory='false' type='String' readonly='false' catalogCategory='Overall'>
                   <instructions><![CDATA[]]></instructions>
                   <value><![CDATA[]]></value>
                </parameter>
             </entry>
             <entry>
-               <string><![CDATA[overall.cpu]]></string>
-               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='overall.cpu' description='Overall available CPUs (cores)' category='General' mandatory='true' type='String' readonly='false' catalogCategory='Overall'>
+               <string><![CDATA[stratuslab.overall.cpu]]></string>
+               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='stratuslab.overall.cpu' description='Overall available CPUs (cores)' category='General' mandatory='true' type='String' readonly='false' catalogCategory='Overall'>
                   <instructions><![CDATA[]]></instructions>
                   <value><![CDATA[]]></value>
                </parameter>
             </entry>
             <entry>
-               <string><![CDATA[single.vm.max.ram]]></string>
-               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='single.vm.max.ram' description='Maximum number of RAM (GB) for a single VM' category='General' mandatory='true' type='String' readonly='false' catalogCategory='Single_VM'>
+               <string><![CDATA[stratuslab.single.vm.max.ram]]></string>
+               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='stratuslab.single.vm.max.ram' description='Maximum number of RAM (GB) for a single VM' category='General' mandatory='true' type='String' readonly='false' catalogCategory='Single_VM'>
                   <instructions><![CDATA[]]></instructions>
                   <value><![CDATA[]]></value>
                </parameter>
             </entry>
             <entry>
-               <string><![CDATA[general.description]]></string>
-               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='general.description' description='General description of the cloud service (including optional further links)' category='General' mandatory='false' type='String' readonly='false' catalogCategory='General'>
+               <string><![CDATA[stratuslab.general.description]]></string>
+               <parameter class='com.sixsq.slipstream.persistence.ServiceCatalogParameter' name='stratuslab.general.description' description='General description of the cloud service (including optional further links)' category='General' mandatory='false' type='String' readonly='false' catalogCategory='General'>
                   <instructions><![CDATA[]]></instructions>
                   <value><![CDATA[]]></value>
                </parameter>
@@ -109,5 +109,5 @@
          </parameters>
       </serviceCatalog>
    </serviceCatalogues>
-   <user issuper='true' resourceUri='user/super' name='super'></user>
+   <user issuper='true' resourceUri='user/meb' name='meb'></user>
 </welcome>")))
