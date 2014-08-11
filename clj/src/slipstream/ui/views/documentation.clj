4e82f574ebe1c85d098050598790ba963e5bf890
rbf <rob@sixsq.com>
2014-08-11 13:41:14 +0200

    [WIP] refactor: remove documentation legacy code

--- clj/src/slipstream/ui/views/documentation.clj
+++ clj/src/slipstream/ui/views/documentation.clj
@@ -1,39 +1,7 @@
 (ns slipstream.ui.views.documentation
-  (:require [clojure.string :as string]
-            [net.cgrand.enlive-html :as html]
+  (:require [slipstream.ui.views.base :as base]
             [slipstream.ui.views.util.icons :as icons]
-            [slipstream.ui.views.tables :as t]
-            [slipstream.ui.views.common :as common]
-            [slipstream.ui.models.common :as common-model]
-            [slipstream.ui.models.module :as module-model]
-            [slipstream.ui.models.user :as user-model]
-            [slipstream.ui.views.common :as common]
-            [slipstream.ui.views.module-base :as module-base]
-            [slipstream.ui.views.header :as header]
-                        [slipstream.ui.views.base :as base]))
-
-(def documentation-template-html (common/get-template "documentation.html"))
-
-(html/defsnippet header-snip documentation-template-html header/header-sel
-  [user]
-  header/header-top-bar-sel (html/substitute
-                              (header/header-top-bar-snip
-                                (user-model/attrs user))))
-
-(html/defsnippet content-snip documentation-template-html common/content-sel
-  []
-  common/content-sel identity)
-
-(defn page-legacy
-  [user]
-  (base/base 
-    {:title (common/title "Documentation")
-     :header (header-snip (user-model/user user))
-     :content (content-snip)}))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+            [slipstream.ui.views.tables :as t]))
 
 (def ^:private docs
   [{:title "User Guide and Tutorial"  :basename "tutorial"}
