0a8c874981d8358f8356b32273ddef3c4c80957c
rbf <rob@sixsq.com>
2014-09-29 16:10:27 +0200

    refactor: remove unused comments

--- clj/src/slipstream/ui/models/module/deployment.clj
+++ clj/src/slipstream/ui/models/module/deployment.clj
@@ -2,32 +2,6 @@
   (:require [net.cgrand.enlive-html :as html]
             [slipstream.ui.util.clojure :as uc]))
 
-; ;; Runs metadata section
-
-; (defn- parse-run
-;   [run-metadata]
-;   (let [attrs (:attrs run-metadata)]
-;     (-> attrs
-;         (select-keys [:tags
-;                       :status
-;                       :uuid
-;                       :username])
-;         (assoc        :start-time  (:starttime attrs))
-;         (assoc        :module-uri  (:moduleresourceuri attrs))
-;         (assoc        :uri         (:resourceuri attrs))
-;         (assoc        :cloud-name  (:cloudservicename attrs)))))
-
-; (defn- group-runs
-;   [runs]
-;   (uc/coll-grouped-by :cloud-name runs
-;                       :items-keyword :runs))
-
-; (defn- runs
-;   [metadata]
-;   (->> (html/select metadata [:runs :item])
-;        (map parse-run)
-;        group-runs))
-
 (defn- parse-node
   [node-metadata]
   (let [attrs (:attrs node-metadata)]
@@ -39,7 +13,4 @@
 (defn sections
   [metadata]
   {:nodes (->> (html/select metadata [:nodes :entry :node])
-               (map parse-node))
-   ; :runs (runs metadata)
-   
-   })
+               (map parse-node))})
