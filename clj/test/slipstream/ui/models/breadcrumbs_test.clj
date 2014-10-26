b2cd1de132de0f69846217fabfa558319c50056e
rbf <rob@sixsq.com>
2014-10-26 19:55:01 +0100

    fix: breadcrumbs test

--- clj/test/slipstream/ui/models/breadcrumbs_test.clj
+++ clj/test/slipstream/ui/models/breadcrumbs_test.clj
@@ -3,29 +3,34 @@
         [slipstream.ui.models.breadcrumbs]))
 
 (expect
-  [{:text "module"}
-   {:text "examples" :uri "module/examples"}
+   [{:text "a" :uri "a"}
+    {:text "b" :uri "a/b"}
+    {:text "c" :uri "a/b/c"}
+    {:text "d" :uri "a/b/c/d"}
+    {:text "e", :uri nil}]
+  (parse "a/b/c/d/e"))
+
+(expect
+  [{:text "examples" :uri "module/examples"}
    {:text "tutorials" :uri "module/examples/tutorials"}
    {:text "wordpress" :uri "module/examples/tutorials/wordpress"}
    {:text "wordpress" :uri "module/examples/tutorials/wordpress/wordpress"}
-   {:text "180"}]
+   {:text "180", :uri nil}]
   (parse "module/examples/tutorials/wordpress/wordpress/180"))
 
 (expect
-  [{:text "module"}
-   {:text "examples" :uri "/module/examples"}
+  [{:text "examples" :uri "/module/examples"}
    {:text "tutorials" :uri "/module/examples/tutorials"}
    {:text "wordpress" :uri "/module/examples/tutorials/wordpress"}
    {:text "wordpress" :uri "/module/examples/tutorials/wordpress/wordpress"}
-   {:text "180"}]
+   {:text "180", :uri nil}]
   (parse "/module/examples/tutorials/wordpress/wordpress/180"))
 
 (expect
-  [{:text "module"}
-   {:text "examples" :uri "module/examples"}
+  [{:text "examples" :uri "module/examples"}
    {:text "tutorials" :uri "module/examples/tutorials"}
    {:text "wordpress" :uri "module/examples/tutorials/wordpress"}
    {:text "wordpress" :uri "module/examples/tutorials/wordpress/wordpress"}
    {:text "180" :uri "module/examples/tutorials/wordpress/wordpress/180"}
-   {:text ""}]
+   {:text "", :uri nil}]
   (parse "module/examples/tutorials/wordpress/wordpress/180/"))
