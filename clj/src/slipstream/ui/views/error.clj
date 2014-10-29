7245e2396e5a07a161fd6817f9be223c09c17a95
rbf <rob@sixsq.com>
2014-10-29 12:57:54 +0100

    change: include exception pseudo id into error page
    This helps identifying exceptions without exposing internal information.

--- clj/src/slipstream/ui/views/error.clj
+++ clj/src/slipstream/ui/views/error.clj
@@ -18,6 +18,23 @@
                           (404 500 nil) (str (t :header.subtitle) "<br><br>" message)
                           message)}}))
 
+(defn- expection-pseudo-hash
+  "Returns a number (as a string) to help identifying exceptions. It is builded
+  using the last line number of each stack trace element, from down to top, preceded
+  by the count of the stack trace elements. Is not ensured to be unique for each
+  exception, but close enough for purposes of support and debugging."
+  [^Throwable t]
+  (let [stack-trace-elements (.getStackTrace t)]
+    (->> stack-trace-elements
+         (into (list))
+         (map str)
+         (map #(re-matches #".*:(\d+)\).*" %))  ; get line numbers
+         (map second)
+         (map last)                             ; get last digit of line number
+         reverse
+         (take 20)
+         (apply str "Exception id: "(count stack-trace-elements) "x"))))
+
 (defn page-uncaught-exception
   [^Throwable t]
-  (page nil (str t) 500))
+  (page nil (str t "<br><br>" (expection-pseudo-hash t)) 500))
