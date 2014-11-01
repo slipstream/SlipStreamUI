7c101218f444354308924e3ac5ea96644ed29ccc
rbf <rob@sixsq.com>
2014-11-01 10:45:45 +0100

    [WIP] change: run page refresh to 1min (instead of 10secs) for dev

--- clj/src/slipstream/ui/views/js/run.js
+++ clj/src/slipstream/ui/views/js/run.js
@@ -300,7 +300,8 @@ jQuery( function() { ( function( $$, $, undefined ) {
             console.log("Updating run page...");
             run.updateParameters();
             run.updateReports();
-            setTimeout("SlipStream.run.autoupdateRunPage()", 10000);
+            setTimeout("SlipStream.run.autoupdateRunPage()", 60000);
+            // setTimeout("SlipStream.run.autoupdateRunPage()", 10000);
         }
     };
 
