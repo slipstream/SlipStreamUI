d609ff956a74c862fc513d7fa11af8dd70bee4f7
rbf <rob@sixsq.com>
2014-11-08 16:51:13 +0100

    fix: remove implemented TODO comment

--- clj/src/slipstream/ui/util/dev.clj
+++ clj/src/slipstream/ui/util/dev.clj
@@ -1,7 +1,6 @@
 (ns slipstream.ui.util.dev
   "Util functions for dev purposes, not prod.")
 
-;; TODO: Use it for localization.clj:58
 (def ^:dynamic *dev?* false)
 
 (defmacro with-dev-environment
