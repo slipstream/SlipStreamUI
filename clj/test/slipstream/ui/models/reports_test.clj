fce7cd2eb1239a34dffcb13b5ab6e3987884e23d
rbf <rob@sixsq.com>
2014-12-08 19:21:22 +0100

    fix: reports refresh in run page
    @mebster @schaubl
    
    Note that the refresh is done together with the refresh of the runtime
    parameters, which happens every 15 secs only when the browser is in
    use (i.e. has focus) and it stops when the run enters a final state.
    
    Further, each time the browser is activated (i.e. gains focus) the
    refresh is re-triggered again, but if the run is in a final state, the
    refresh will only run once until the browser is focused again.

--- clj/test/slipstream/ui/models/reports_test.clj
+++ clj/test/slipstream/ui/models/reports_test.clj
@@ -4,24 +4,16 @@
             [slipstream.ui.models.reports :as model]))
 
 (def raw-metadata-str
-  "{
-    \"files\": [
-        {
-            \"date\": \"2014-07-05T080220Z\",
-            \"name\": \"orchestrator-ec2-eu-west_report_2014-07-05T080220Z.tgz\",
-            \"node\": \"orchestrator-ec2-eu-west\",
-            \"type\": \"tgz\",
-            \"uri\": \"http://slipstream.sixsq.com/reports/91aac79a-7090-4fb6-861d-9cbf6101b5b8/orchestrator-ec2-eu-west_report_2014-07-05T080220Z.tgz\"
-        },
-        {
-            \"date\": \"2014-07-05T080217Z\",
-            \"name\": \"wp.1_report_2014-07-05T080217Z.tgz\",
-            \"node\": \"wp.1\",
-            \"type\": \"tgz\",
-            \"uri\": \"http://slipstream.sixsq.com/reports/91aac79a-7090-4fb6-861d-9cbf6101b5b8/wp.1_report_2014-07-05T080217Z.tgz\"
-        }
-    ]
-}")
+  "<html>
+    <head></head>
+    <body style=\"font-family: sans-serif;\">
+      <h2>Listing of \"/reports/91aac79a-7090-4fb6-861d-9cbf6101b5b8\"</h2>
+      <a href=\"http://slipstream.sixsq.com/reports/\">..</a>
+      <br>
+      <a href=\"http://slipstream.sixsq.com/reports/91aac79a-7090-4fb6-861d-9cbf6101b5b8/orchestrator-ec2-eu-west_report_2014-07-05T080220Z.tgz\">orchestrator-ec2-eu-west_report_2014-07-05T080220Z.tgz</a>
+      <a href=\"http://slipstream.sixsq.com/reports/91aac79a-7090-4fb6-861d-9cbf6101b5b8/wp.1_report_2014-07-05T080217Z.tgz\">wp.1_report_2014-07-05T080217Z.tgz</a>
+      <br></body>
+    </html>")
 
 (expect
   [{:date "2014-07-05T080220Z"
