735df89cddaf912ec1ffd8ce7e6fd313fdde322a
rbf <rob@sixsq.com>
2014-12-15 01:35:52 +0100

    change: node reference image link not open in new page in view mode

--- clj/src/slipstream/ui/views/tables.clj
+++ clj/src/slipstream/ui/views/tables.clj
@@ -553,7 +553,7 @@
       :cells [{:type :cell/text,              :content (t :reference-image.label)}
               {:type :cell/reference-module,  :content {:value (:reference-image deployment-node)
                                                         :colspan 2
-                                                        :open-in-new-window? true}, :editable? false}
+                                                        :open-in-new-window? (page-type/edit-or-new?)}, :editable? false}
               {:type :cell/hidden-input,      :content {:value (-> deployment-node :reference-image u/module-uri (uc/trim-prefix "/"))
                                                         :id (format "node--%s--imagelink" node-index)}}]}
      {:class "ss-deployment-node-default-multiplicity-row"
