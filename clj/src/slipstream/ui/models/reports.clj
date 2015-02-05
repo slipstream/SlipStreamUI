d44b4eed44c017193951126fa28309032423b472
rbf <rob@sixsq.com>
2015-02-05 16:00:31 +0100

    fix: revert to usage of json for run reports metadata
    Revert commit "fix: reports refresh in run page"
    Commit hash: fce7cd2eb1239a34dffcb13b5ab6e3987884e23d.
    
    @konstan
    please check now
    
    @mebster
    unless I miss something, the UI cannot request a specific metadata
    format (XML or JSON)
    therefore, if the server provides another format, the UI cannot know
    automatically

--- clj/src/slipstream/ui/models/reports.clj
+++ clj/src/slipstream/ui/models/reports.clj
@@ -1,6 +1,4 @@
-(ns slipstream.ui.models.reports
-  (:require [net.cgrand.enlive-html :as html]
-            [slipstream.ui.util.clojure :as uc]))
+(ns slipstream.ui.models.reports)
 
 (defn- strip-domain
   [url]
@@ -8,24 +6,12 @@
     relative-uri
     url))
 
-(def ^:private date-re #".*(20\d{2}-\d{2}-\d{2}T\d{6}Z).*") ; "2014-07-05T080220Z"
-(def ^:private type-re #".*\.(.*)")
-(def ^:private name-re #".*\/(.*)")
-(def ^:private node-re #".*\/(.*?)_.*")
-
-(defn- parse-href
-  [href]
-  {:date          (->> href (re-matches date-re) second)
-   :name          (->> href (re-matches name-re) second)
-   :node          (->> href (re-matches node-re) second)
-   :type          (->> href (re-matches type-re) second)
-   :uri           href
-   :relative-uri  (strip-domain href)})
+(defn- assoc-relative-uri
+  [{:keys [uri] :as m}]
+  (assoc m :relative-uri (strip-domain uri)))
 
 (defn parse
   [metadata]
-  (->> (html/select metadata [:a])
-       (map :attrs)
-       (map :href)
-       (map parse-href)
-       (filter #(-> % :name not-empty))))
+  (->> metadata
+       :files
+       (map assoc-relative-uri)))
