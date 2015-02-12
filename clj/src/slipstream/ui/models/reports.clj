b8fa7dd7f64f5e46aefff9531c3c271e2edc59ff
rbf <rob@sixsq.com>
2015-02-12 12:12:20 +0100

    Revert "fix: revert to usage of json for run reports metadata", #173
    This reverts commit d44b4eed44c017193951126fa28309032423b472.
    
    @schaubl
    the server sends still XML metadata to the UI module, my bad
    can you please test this with real reports data?
    cheers

--- clj/src/slipstream/ui/models/reports.clj
+++ clj/src/slipstream/ui/models/reports.clj
@@ -1,4 +1,6 @@
-(ns slipstream.ui.models.reports)
+(ns slipstream.ui.models.reports
+  (:require [net.cgrand.enlive-html :as html]
+            [slipstream.ui.util.clojure :as uc]))
 
 (defn- strip-domain
   [url]
@@ -6,12 +8,24 @@
     relative-uri
     url))
 
-(defn- assoc-relative-uri
-  [{:keys [uri] :as m}]
-  (assoc m :relative-uri (strip-domain uri)))
+(def ^:private date-re #".*(20\d{2}-\d{2}-\d{2}T\d{6}Z).*") ; "2014-07-05T080220Z"
+(def ^:private type-re #".*\.(.*)")
+(def ^:private name-re #".*\/(.*)")
+(def ^:private node-re #".*\/(.*?)_.*")
+
+(defn- parse-href
+  [href]
+  {:date          (->> href (re-matches date-re) second)
+   :name          (->> href (re-matches name-re) second)
+   :node          (->> href (re-matches node-re) second)
+   :type          (->> href (re-matches type-re) second)
+   :uri           href
+   :relative-uri  (strip-domain href)})
 
 (defn parse
   [metadata]
-  (->> metadata
-       :files
-       (map assoc-relative-uri)))
+  (->> (html/select metadata [:a])
+       (map :attrs)
+       (map :href)
+       (map parse-href)
+       (filter #(-> % :name not-empty))))
