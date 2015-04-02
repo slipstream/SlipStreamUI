3bc4a7cdb8a4204c10ffef27b035ae71050e2a52
rbf <rob@sixsq.com>
2015-04-02 19:21:04 +0200

    new: include test for bug fixed in previous commit fe29335

--- clj/test/slipstream/ui/models/vms_super_test.clj
+++ clj/test/slipstream/ui/models/vms_super_test.clj
@@ -9,7 +9,7 @@
       <vm cloud='CloudA' user='bob' state='Running' instanceId='4f2708e1-6aa6-4469-8f5e-b18ffd42cb50_machine' measurement='2014-12-08 13:09:24.82 CET' runUuid='a1b345f0-c434-490e-849f-c3894af55588' ip='127.0.0.1' name='machine' runOwner='super'/>
       <vm cloud='CloudA' user='bob' state='Terminated' instanceId='157701e9-9c52-45b1-a506-13241446aad3_machine' measurement='2014-12-08 13:09:24.98 CET' runUuid='a1b345f0-c434-490e-849f-c3894af55588' name='machine' runOwner='bob'/>
       <vm cloud='CloudA' user='bob' state='Running' instanceId='vm_gateway' measurement='2014-12-08 13:09:24.112 CET' runUuid='35c3789f-5da1-4504-8294-d41489d035ae' ip='127.0.0.1' name='vm_gateway' runOwner='super'/>
-      <vm cloud='CloudA' user='bob' state='Running' instanceId='vApp_6_centos' measurement='2014-12-08 13:09:24.126 CET' runUuid='Unknown'/>
+      <vm cloud='CloudA' user='bob' state='Running' instanceId='vApp_6_centos' measurement='2014-12-08 13:09:24.126 CET'/>
       <vm cloud='CloudA' user='bob' state='Running' instanceId='1b0afd4f-340d-4bef-89e2-d6f4a776f2ea' measurement='2014-12-08 13:09:33.215 CET' runUuid='Unknown'/>
       <user issuper='true' resourceUri='user/super' name='super'></user>
   </vms>")
