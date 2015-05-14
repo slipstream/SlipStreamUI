ea6412ca314325b5d6867ecc8a657317a0aab23f
rbf <rob@sixsq.com>
2015-05-14 16:53:16 +0200

    new: warning alerts in user profile view page for missing infos, #238
    If no cloud is configured, a (fixed) alert is displayed in the user
    profile page (only in view mode, not edit).
    
    When looking at the profile page of somebody else (i.e. being super),
    there is an additional warning alert if there is no SSH key configured.

--- clj/src/slipstream/ui/views/user.clj
+++ clj/src/slipstream/ui/views/user.clj
@@ -36,9 +36,30 @@
                   (:super? user)    (t :subtitle.super)
                   :else             (t :subtitle.not-super))}))
 
+(defn- no-cloud-configured-alert
+  [user own-profile?]
+  (when-not (-> user :configuration :configured-clouds)
+    {:type      :warning
+     :container :fixed
+     :title     (t (if own-profile?
+                     :alert.no-clouds-configured.own-profile.title
+                     :alert.no-clouds-configured.others-profile.title))
+     :msg       (t (if own-profile?
+                     :alert.no-clouds-configured.own-profile.msg
+                     :alert.no-clouds-configured.others-profile.msg))}))
+
+(defn- no-ssh-keys-configured-alert
+  [user own-profile?]
+  (when-not (or own-profile? (-> user :configuration :ssh-keys))
+    {:type      :warning
+     :container :fixed
+     :title     (t :alert.no-ssh-keys-configured.others-profile.title)
+     :msg       (t :alert.no-ssh-keys-configured.others-profile.msg)}))
+
 (defn page
   [metadata]
-  (let [user (user/parse metadata)]
+  (let [user (user/parse metadata)
+        own-profile? (current-user/is? user)]
     (base/generate
       {:parsed-metadata user
        :header (header user)
@@ -46,6 +67,9 @@
        :secondary-menu-actions [action/edit
                                 action/delete]
        :resource-uri (if (current-user/super?) (:uri user) (t :header.title.loggedin))
+       :alerts (when (page-type/view?)
+                 [(no-cloud-configured-alert    user own-profile?)
+                  (no-ssh-keys-configured-alert user own-profile?)])
        :content (into [{:icon       icons/user-section-summary
                         :title      (t :summary)
                         :selected?  true
