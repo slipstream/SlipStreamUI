73a8511bf005dd63595c8617580e6f97e2cfbef1
rbf <rob@sixsq.com>
2015-06-01 18:41:43 +0200

    Revert "fix: allow tags to be modified during the tour, #238"
    This reverts commit f36e49656406006c89e1be73c74c9df86341899d.
    
    It more clear to explicitly catch the exception that a recurrent job is
    missing in a per case basis. (See next commit)

--- clj/resources/static_content/js/util.js
+++ clj/resources/static_content/js/util.js
@@ -2233,15 +2233,9 @@ jQuery( function() { ( function( $$, util, $, undefined ) {
         },
 
         stop: function(name){
-            var job;
-            try {
-                job = this.getJob(name);
-                clearTimeout(job.timeoutID);
-                job.timeoutID = 0;
-            } catch (e) {
-                // A job with this name might not exist
-                console.warn(e);
-            }
+            var job = this.getJob(name);
+            clearTimeout(job.timeoutID);
+            job.timeoutID = 0;
             return this;
         },
 
