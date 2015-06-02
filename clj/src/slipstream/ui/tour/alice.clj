ea988dd4baf19b996c3b89db8c5f5d1611da5bd8
Charles (Cal) Loomis <cal@sixsq.com>
2015-06-02 16:48:31 +0200

    english updates

--- clj/src/slipstream/ui/tour/alice.clj
+++ clj/src/slipstream/ui/tour/alice.clj
@@ -20,7 +20,7 @@
    [
       :#ss-section-group-0
       {:title "Main sections"
-       :content (str "There are three main section on the welcome page of SlipStream. "
+       :content (str "There are three main sections on the welcome page of SlipStream."
                      "<ol><li>AppStore</li><li>Projects</li><li>Service Catalog</li></ol>")}
 
       [:#ss-section-group-0 :> html/first-child]
@@ -30,27 +30,27 @@
 
       [:#ss-section-app-store :> :div :> :div :> [:div (ue/first-of-class "ss-example-app-in-tour")] :> :div]
       {:title "Application"
-       :content "This is a plublished application. Click 'next' to learn how to deploy it."}
+       :content "This is a published application. Click 'next' to learn how to deploy it."}
 
       [:#ss-section-app-store :> :div :> :div :> [:div (ue/first-of-class "ss-example-app-in-tour")] :> :div :.ss-app-image-container]
       {:title "Deploy"
-       :content "Click the <span style='color:#fff;background-color:#337ab7;padding: 4px 8px;font-weight:normal;'><span class='glyphicon glyphicon-cloud-upload'></span> Deploy</span> button in the bottom right part of the application logo to deploy."
+       :content "Click the <span style='color:#fff;background-color:#337ab7;padding: 4px 8px;font-weight:normal;'><span class='glyphicon glyphicon-cloud-upload'></span> Deploy</span> button in the bottom right part of the application logo."
        :width "300px"}
     ]
 
    :deploying-wordpress
    [
       nil
-      {:title "Run Dialog"
-       :content (str "We are now in the page of the WordPress SlipStream image and the <span class='panel-primary'><h4 class='modal-header panel-heading'>Run image</h4></span> dialog behind has been automatically open."
+      {:title "Run dialog"
+       :content (str "We are now on the WordPress SlipStream image page and the <span class='panel-primary'><h4 class='modal-header panel-heading'>Run image</h4></span> dialog behind has been automatically opened."
                      " If you need to open it again, click on the menu action <span style='color:#54A9DE;background-color:#393939;font-weight:400;padding:2px 8px;'><span class='glyphicon glyphicon-cloud-upload' style='display:inline;'></span>&nbsp;Run...</span> in the right part of the page."
-                     " In this dialog you can specify some parameters for the deployment."
+                     " In this dialog you can specify the values of some parameters for the deployment."
                      "<br/><br/>"
                      "Click " next-button-label " to learn how to configure and launch WordPress.")}
 
       [:#ss-run-module-dialog #{:#parameter--cloudservice :#global-cloud-service}]
       {:title "Choose the cloud"
-       :content "Please choose where you want WordPress to be deployed. Please note that you can only choose the clouds for which you have the corresponding credentials configured in your profile."
+       :content "Please choose where you want WordPress to be deployed. Please note that you can only choose those clouds for which you have provided credentials in your profile."
        :container-sel "#ss-run-module-dialog"
        :preserve-padding true
        :placement "right"
@@ -58,7 +58,7 @@
 
       [:#ss-run-module-dialog :#tags]
       {:title "Give it a name"
-       :content "You can assing some <code>tags</code> to the deployment. This will be useful to recognise it later on. Try something like <code>wp-tour-test</code>. Don't worry, you will be able to update it later on."
+       :content "You can assign a <code>tag</code> to the deployment. This will be useful to recognise it later on. Try something like <code>wp-tour-test</code>. Don't worry, you can change it later."
        :container-sel "#ss-run-module-dialog"
        :preserve-padding true
        :placement "right"
@@ -84,13 +84,13 @@
 
       :#header-content
       {:title "At a glance"
-       :content "In the header you can have an overview of the main info like the run id, the state, who and when started it and the type of run."
+       :content "In the header you have an overview with information like the run id, the state, who started it, when it was started, and the type of run."
        :placement "bottom"}
 
       :#ss-breadcrumb-container
       {:title "The breadcrumbs"
-       :content (str "Across the whole application you can rely in the breadcrumbs to know where you are and navigate up the hierarchy."
-                     " E.g. clicking on the <span class='glyphicon glyphicon-home'></span> will bring you always to the welcome page , with the App Store."
+       :content (str "Throughout SlipStream you can rely on the breadcrumbs to know where you are and navigate up the hierarchy."
+                     " E.g. clicking on the <span class='glyphicon glyphicon-home'></span> will always bring you to the welcome page containing the App Store."
                      "<br/><br/>"
                      "In this case, you can see which module (i.e. <code>wordpress</code>) and version was deployed and where it's located in the project tree.")
        :width "100%"
@@ -99,28 +99,28 @@
 
       [:#ss-section-group-0 :> :div.panel.ss-section-selected.ss-section.panel-default]
       {:title "Overview"
-       :content (str "This offers a graphical overview of the running machines and global info about each one, like state, IP address and custom message."
-                     " Hover on the different nodes to reveal more details.")
+       :content (str "This offers a graphical overview of the running machines and global information about each one, like state, IP address and custom message."
+                     " Hover over the different nodes to reveal more details.")
        :placement "top"}
 
       [:#ss-section-group-0 :> [:div.panel.ss-section.panel-default (html/nth-child 2)]]
       {:title "Summary"
-       :content (str "This section summarizes the top level information about the run: who and when started what."
+       :content (str "This section summarizes the top level information about the run."
                      "<br/><br/>"
-                     "Note that here you can add and modify the runs <code>tags</code>, as we promised in the previous page! &#x1F60E;") ;; &#x1F60E; is :sunglasses: emoji
+                     "Note that here you can add and modify the <code>tags</code>, as we promised in the previously! &#x1F60E;") ;; &#x1F60E; is :sunglasses: emoji
        :placement "top"}
 
       [:#ss-section-group-0 :> [:div.panel.ss-section.panel-default (html/nth-child 3)]]
       {:title "Global run parameters"
-       :content (str "This and the following sections contain a lot of detailed information about the run and the deployed machines. Here you can find out IP adresses, error causes and other parameters."
+       :content (str "This and the following sections contain a lot of detailed information about the run and the deployed machines. Here you can find machine IP addresses, deployment errors, and other parameters."
                      "<br/><br/>"
-                     "Across the whole application you can hover on the <span class='glyphicon glyphicon-question-sign'></span> to reveal a detailed explanation of the corresponding item.")
+                     "Throughout SlipStream you can hover over the <span class='glyphicon glyphicon-question-sign'></span> to reveal a detailed explanation of the corresponding item.")
        :placement "top"}
 
       [:#ss-section-group-0 :> [:div.panel.ss-section.panel-default html/last-child]]
       {:title "Reports"
        :content (str "For each run, SlipStream collects a series of reports that you will find here."
-                     " You might want to come here and download them specially if your run didn't worked as expected."
+                     " You might want to come here and download them specially if your run didn't work as expected."
                      "<br/><br/>"
                      "Note that you don't need to reload the page: the reports will automatically appear here when available.")
        :placement "top"}
@@ -132,11 +132,11 @@
 
       [:#topbar :> :div :> :div :> :div.navbar-collapse.collapse :> :ul :> [:li (html/nth-child 1)]]
       {:title "Dashboard"
-       :content (str "The deployment will take some minutes, depending on the cloud you selected and its current load."
+       :content (str "The deployment will take some time, depending on the cloud you selected and its current load."
                      "<br/><br/>"
-                     "In the meantime we will visit the dashboard, also a very central point of SlipStream where you will have an overview of the applications you have running, including this WordPress you just launched."
+                     "In the meantime we will visit the dashboard, also a very central point of SlipStream where you will have an overview of the applications you have running, including the WordPress instance you just launched."
                      "<br/><br/>"
-                     "Click on the <span style='color:white;background-color:#a00;font-weight:normal;padding:2px 8px;'>Dashboard</span> above to discover in. We will come back here afterwards to see how the run finished.")
+                     "Click on the <span style='color:white;background-color:#a00;font-weight:normal;padding:2px 8px;'>Dashboard</span> above to discover it. We will come back here afterwards to see how the run finished.")
        :placement "bottom"}
     ]
 
@@ -151,7 +151,7 @@
 
       [[:div.panel.ss-section.panel-default (html/nth-child 1)]]
       {:title "Usage section"
-       :content "Here you see all clouds that are you can have access to and your resource comsumption on each one."
+       :content "Here you see all of the accessible clouds and your resource comsumption on each one."
        :placement "top"}
 
       (->> :cloud (query-param-value context) (str "#ss-usage-gauge-") keyword)
@@ -165,19 +165,19 @@
 
       [[:div.panel.ss-section.panel-default (html/nth-child 3)]]
       {:title "VMs section"
-       :content (str "This are all the individual VMs that are launched using your cloud credentials (i.e. a <code>describe-instance</code> kind of info, if it speaks to you)."
-                     " Note that if some VMs were launched using with your same credentials, they will also be listed here, but without a <code>Run Id</code>, since SlipStream doesn't know them.")
+       :content (str "These are all the individual VMs that are launched using your cloud credentials (i.e. a <code>describe-instance</code> kind of info, if it speaks to you)."
+                     " Note that if some VMs were launched outside of SlipStream with your credentials, they will also be listed here, but without a <code>Run Id</code>.")
        :placement "top"}
 
       [[:div.panel.ss-section.panel-default (html/nth-child 4)]]
       {:title "Metering section"
-       :content "Here you can see the history of your consumption in several time frames."
+       :content "Here you can see the history of your consumption over different periods of time."
        :placement "top"}
 
       [[:div.panel.ss-section.panel-default (html/nth-child 2)]]
       {:title "Runs section"
        :content (str "Here you can find all the runs grouped by cloud, including the WordPress you launched before."
-                     " Click on its <code>Run Id</code> (we kindly noted it for you: <code>" (query-param-value context :wordpress-run-id) "</code>) to go back to the <code>Run page</code> we where before."
+                     " Click on its <code>Run Id</code> (we kindly noted it for you: <code>" (query-param-value context :wordpress-run-id) "</code>) to go back to the <code>Run page</code>."
                      " Note that you can identify your run also with the <code>tag</code> you used.")
        :placement "top"}
     ]
@@ -192,7 +192,7 @@
         nil
         {
          :title "First things first"
-         :content (str "To begin using SlipStream you need to tell it where to deploy your applications to. "
+         :content (str "To begin using SlipStream you need to tell it where to deploy your applications. "
                        "For that, you need to have an account with at least one cloud provider and set up your credentials in your SlipStream profile."
                        "<br/><br/>"
                        "Click " next-button-label " to begin the tour by configuring one (or more!) clouds.")
@@ -202,14 +202,14 @@
         {
          :title "Cloud credentials"
          :content (str "If you already have an account with a cloud provider, just prepare its credentials (usually a <code>user/password</code> or a <code>key/secret</code> pair). "
-                       "If not, please create one following the indications in our <a target='_blank' href='http://ssdocs.sixsq.com/documentation/advanced_tutorial/accounts.html#cloud-infrastructure-accounts'>documentation</a>. "
+                       "If not, please create one following the procedure in our <a target='_blank' href='http://ssdocs.sixsq.com/documentation/advanced_tutorial/accounts.html#cloud-infrastructure-accounts'>documentation</a>. "
                        "<br/><br/>"
                        "When your cloud account is ready, go to the next step to learn how to set the credentials in your SlipStream user profile.")
          }
 
         :#ss-menubar-user-profile-anchor
         {:title "Go to your profile"
-         :content "Let's configure your first cloud account. Go to your user profile clicking this menu here. Note that you are also able to logout from this menu."
+         :content "Let's configure your first cloud account. Go to your user profile clicking this menu item. Note that you are also able to logout from this menu."
          :placement "left"
          :container-sel "body"
          :preserve-padding true
@@ -230,19 +230,19 @@
 
         [:#ss-section-group-0 :> :div.panel.ss-section-selected.ss-section.panel-default]
         {:title "Edit your personal information"
-         :content "First of all, please take this moment to make sure that your information is correct."
+         :content "First of all, please take a moment to make sure that your information is correct."
          :placement "top"
          }
 
         [:#ss-section-group-1]
         {:title "Cloud credentials"
-         :content "This is the configuration section for your cloud accounts. Please enter your credentials for at least one cloud here."
+         :content "This is the configuration section for your cloud accounts. Please enter credentials for at least one cloud here."
          :placement "top"
          }
 
         :#ss-secondary-menu-action-save
         {:title "Save your profile"
-         :content "Click here to... exactly: save the chages you just made."
+         :content "Click here to... exactly: save the changes you just made."
          :placement "bottom"
          :preserve-padding true
          }
@@ -252,7 +252,7 @@
      [
         [:#topbar :> :div :> :div :> :div.navbar-header :> :a]
         {:title "Back to the main page"
-         :content "Now that you have at least one cloud configured, click on the logo to back to the AppStore on the main page and select your first app to deploy."
+         :content "Now that you have at least one cloud configured, click on the SlipStream logo to back to the AppStore on the main page."
          :placement "bottom"
          :preserve-padding true
          }
