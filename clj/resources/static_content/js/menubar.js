773ba7b812af88444a4059bb6c97959096bd56e8
rbf <rob@sixsq.com>
2015-07-22 19:08:29 +0200

    fix: make wrong credentials alert visible on mobile, #226

--- clj/resources/static_content/js/menubar.js
+++ clj/resources/static_content/js/menubar.js
@@ -1,9 +1,23 @@
 jQuery( function() { ( function( $$, $, undefined ) {
 
+    $(".ss-login-error-on-mobile-alert")
+        .click(function (e) {
+            e.preventDefault();
+            $(this).stop().fadeOut();
+            return false;
+        });
+
     $$.request
         .post("/login")
         .onErrorStatusCodeAlert(401, "Wrong credentials",
                                      "The provided username doesn't exist or the password is incorrect. Please try again.")
+        .onErrorStatusCode(401, function(){
+            var $loginErrorAlert = $(".ss-login-error-on-mobile-alert"),
+                shouldFadeIn = $loginErrorAlert.prev().is(":visible");
+            if ( shouldFadeIn ) {
+                $(".ss-login-error-on-mobile-alert").fadeIn().delay(2000).fadeOut();
+            }
+        })
         .onSuccessFollowRedirectInURL()
         .useToSubmitForm("#login");
 
