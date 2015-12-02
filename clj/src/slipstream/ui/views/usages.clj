f31a24d047968363f9185dfe2e70cb967116b3e5
rbf <rob@sixsq.com>
2015-12-02 13:50:30 +0100

    minor: remove unnecessary debug code
    @st FYI we forgot to remove this debug line.

--- clj/src/slipstream/ui/views/usages.clj
+++ clj/src/slipstream/ui/views/usages.clj
@@ -24,7 +24,7 @@
 (defn- section
   [metadata]
   (let [parsed-metadata (usages/parse metadata)
-        frequency (slipstream.ui.util.dev-traces/>>> some->> metadata meta :request :query-parameters :filter (re-find #"frequency='(daily|weekly|monthly)'") second keyword)]
+        frequency (some->> metadata meta :request :query-parameters :filter (re-find #"frequency='(daily|weekly|monthly)'") second keyword)]
     (if frequency
       [(frequency-subsection parsed-metadata true frequency)]
       [{:title (t :content.title)
