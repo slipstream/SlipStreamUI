1399dfabadbaa5cf023a59f6c3b3f9d6900b32a9
sixsq-hudson <hudson@sixsq.com>
2018-02-04 07:59:29 +0000

    release v3.45

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.45-SNAPSHOT")
+(def +version+ "3.45")
 
-(defproject com.sixsq.slipstream/SlipStreamUI "3.45-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamUI "3.45"
 
   :description "Legacy Web Browser Interface"
 
@@ -13,7 +13,7 @@
   :plugins [[lein-parent "0.3.2"]
             [lein-expectations "0.0.8"]]
 
-  :parent-project {:coords  [com.sixsq.slipstream/parent "3.45-SNAPSHOT"]
+  :parent-project {:coords  [com.sixsq.slipstream/parent "3.45"]
                    :inherit [:min-lein-version
                              :managed-dependencies
                              :repositories
