a7e69991bb78cf69a5e90a99f13c1281d5827b79
sixsq-hudson <hudson@sixsq.com>
2018-08-17 15:15:39 +0000

    release v3.57

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.57-SNAPSHOT")
+(def +version+ "3.57")
 
-(defproject com.sixsq.slipstream/SlipStreamUI "3.57-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamUI "3.57"
 
   :description "Legacy Web Browser Interface"
 
