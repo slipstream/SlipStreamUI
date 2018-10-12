14c0fdec4177f6058d7ba2d4c4772b9bdcebda0a
Eric Le Goff <elegoff@sixsq.com>
2018-10-12 15:42:59 +0200

    update parent version

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
             [lein-expectations "0.0.8"]
             [lein-resource "16.9.1"]]
 
-  :parent-project {:coords  [sixsq/slipstream-parent "5.3.11"]
+  :parent-project {:coords  [sixsq/slipstream-parent "5.3.12"]
                    :inherit [:plugins
                              :min-lein-version
                              :managed-dependencies
