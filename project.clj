a9bb42f9757b3764ba80abb71d79ef16dac0d616
sixsq-hudson <hudson@sixsq.com>
2019-02-01 14:20:39 +0000

    next development version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.68")
+(def +version+ "3.69-SNAPSHOT")
 
-(defproject com.sixsq.slipstream/SlipStreamUI "3.68"
+(defproject com.sixsq.slipstream/SlipStreamUI "3.69-SNAPSHOT"
 
   :description "Legacy Web Browser Interface"
 
